e9ae5ddda57641509d054d9fc23f30dc1f779754
PlayerG9 <g9yout@gmail.com>
2022-07-05 11:53:20 +0200

    react-router is now working

--- src/web-ui/src/App.tsx
+++ src/web-ui/src/App.tsx
@@ -1,8 +1,28 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
 
 export default function App() {
   return <>
     <h1>Hello World</h1>
+    <JourneyRouter />
   </>
+}
+
+
+function JourneyRouter(){
+  return <>
+    <Routes>
+      <Route index element={<>
+        <p>Nah</p>
+        <Link to="/test">ToGo</Link>
+      </>}/>
+      <Route path="/test" element={<TestPage/>}/>
+    </Routes>
+  </>
+}
+
+
+function TestPage(){
+  return <h3>Success</h3>
 }
