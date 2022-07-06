bcbc366c9005e1f88e17a9943da635873faf6ed1
PlayerG9 <g9yout@gmail.com>
2022-07-06 08:46:06 +0200

    added react-query for api calls

--- src/web-ui/src/App/index.tsx
+++ src/web-ui/src/App/index.tsx
@@ -1,9 +1,13 @@
 import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import JourneyRouter from './journeyrouter'
 
 
+const queryClient = new QueryClient()
+
+
 export default function App() {
-    return <>
+    return <QueryClientProvider client={queryClient}>
         <JourneyRouter />
-    </>
+    </QueryClientProvider>
 }
