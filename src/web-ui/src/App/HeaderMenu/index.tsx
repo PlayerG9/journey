ab6c8ccbac366f91f4225ec5eae168578878c1c2
PlayerG9 <g9yout@gmail.com>
2022-07-09 15:59:04 +0200

    improved header

--- src/web-ui/src/App/HeaderMenu/index.tsx
+++ src/web-ui/src/App/HeaderMenu/index.tsx
@@ -1,12 +1,13 @@
 import './index.scss'
 import { Link } from 'react-router-dom'
+import headerIcon from './header-icon.png'
 
 
 export default function HeaderMenu(){
     return <div className='header-menu'>
         <Link className='icon' to="/">
-            <img src="" alt="icon"/>
-            <h1>PlayerG9/Journal</h1>
+            <img src={headerIcon} alt="icon"/>
+            <p>PlayerG9/Journal</p>
         </Link>
     </div>
 }
